6975794b4f82c9a3fa4f1edbfd107c3dd393964d
TienLePham <tienlepham094@gmail.com>
2022-07-05 21:48:58 +0700

    leetcode 5/7

---
+++ Leetcode/Easy/SumOfLeftLeaves.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0;
+        elif root.left !=None and root.left.left == None and root.left.right == None:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+            
